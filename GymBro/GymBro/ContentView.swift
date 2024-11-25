02411271b68b8b96536f35538a12c981125463c3
jgudgin <126836438+jgudgin@users.noreply.github.com>
2024-11-25 18:44:32 +1300

    add tab changing

--- GymBro/GymBro/ContentView.swift
+++ GymBro/GymBro/ContentView.swift
@@ -13,10 +13,27 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("GymBro \n      🏋️‍♂️")
-                .font(.system(size: 80))
+                .font(.system(size: 50))
                 .padding()
             
             Spacer() // Pushes everything above to the top and everything below to the bottom
+            
+            // Content changes based on selected option
+            Group {
+                switch selectedOption {
+                case "Fitness":
+                    FitnessView()
+                case "Social":
+                    SocialView()
+                case "Diet":
+                    DietView()
+                case "Profile":
+                    ProfileView()
+                default:
+                    Text("Select an option")
+                }
+            }
+            .padding()
             
             Picker("Options", selection: $selectedOption) {
                 Text("Fitness").tag("Fitness")
@@ -26,8 +43,44 @@ struct ContentView: View {
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
+            
+            
+
         }
         .edgesIgnoringSafeArea(.bottom) // Extends the VStack to the bottom edge of the screen
+    }
+}
+
+// Individual views for each tab
+struct FitnessView: View {
+    var body: some View {
+        Text("Welcome to Fitness!")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct SocialView: View {
+    var body: some View {
+        Text("Social interactions go here.")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct DietView: View {
+    var body: some View {
+        Text("Diet plans and trackers!")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Your Profile.")
+            .font(.title)
+            .padding()
     }
 }
 
