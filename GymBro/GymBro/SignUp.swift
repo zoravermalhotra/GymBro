7556cce91ae24c7878cb1bed929aaf9bd695ffbe
jgudgin <126836438+jgudgin@users.noreply.github.com>
2024-11-28 20:48:55 +1300

    add fields

--- GymBro/GymBro/SignUp.swift
+++ GymBro/GymBro/SignUp.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct SignUp: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var verifyPassword = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    
+    
     var body: some View {
         VStack {
             Text("Sign Up for GymBro")
@@ -16,7 +23,28 @@ struct SignUp: View {
 
             Spacer()
             
+            TextField("Username", text: $username)
+                .padding()
+                .frame(width: 300, height: 50)
+                .overlay { RoundedRectangle(cornerRadius: 10).stroke(Color.primary) }
+                .border(Color.red, width: CGFloat(wrongUsername)).cornerRadius(10)
+                .autocapitalization(.none)
+
+            SecureField("Password", text: $password)
+                .padding()
+                .frame(width: 300, height: 50)
+                .overlay { RoundedRectangle(cornerRadius: 10).stroke(Color.primary) }
+                .border(Color.red, width: CGFloat(wrongPassword)).cornerRadius(10)
+                .autocapitalization(.none)
             
+            SecureField("Verify Password", text: $verifyPassword)
+                .padding()
+                .frame(width: 300, height: 50)
+                .overlay { RoundedRectangle(cornerRadius: 10).stroke(Color.primary) }
+                .border(Color.red, width: CGFloat(wrongPassword)).cornerRadius(10)
+                .autocapitalization(.none)
+            
+                Spacer()
         }
     }
 }
