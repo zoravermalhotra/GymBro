fb820f70d4b06743d3eed82431fda8ede0c973ef
zoravermalhotra <163698706+zoravermalhotra@users.noreply.github.com>
2024-11-28 19:40:30 +1300

    add login button and basic functionality

--- GymBro/GymBro/Login.swift
+++ GymBro/GymBro/Login.swift
@@ -9,40 +9,74 @@ import SwiftUI
 
 struct Login: View {
     @State var selectedTab = "Login"
-    @State private var username: String = ""
-    @State private var password: String = ""
-    //@State private var button: Button<Label: View>
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var isLoggedIn = false
 
 
     
     var body: some View {
-        VStack {
-            
-            Text("Login")
-                .font(.title)
-                .padding()
-            
-            
-            Spacer()
-            
-            TextField("Username", text: $username)
-                .font(.system(size: 20))
-                .padding()
-                .overlay {RoundedRectangle(cornerRadius: 20).stroke(Color.primary)}
-                .padding(.horizontal)
-                .autocapitalization(.none)
-            
-            
-            TextField("Password", text: $password)
-                .font(.system(size: 20))
-                .padding()
-                .overlay {RoundedRectangle(cornerRadius: 20).stroke(Color.primary)}
-                .padding(.horizontal)
-                .autocapitalization(.none)
-            
-            Spacer()
-
+        NavigationStack {
+            VStack {
                 
+                Text("Login")
+                    .font(.title)
+                    .padding()
+                
+                Spacer()
+                
+                TextField("Username", text: $username)
+                    .padding()
+                    .frame(width: 300,  height: 50)
+                    .overlay {RoundedRectangle(cornerRadius: 10).stroke(Color.primary)}
+                    .border(Color.red, width: CGFloat(wrongUsername)).cornerRadius(10)
+                    .autocapitalization(.none)
+                    
+                
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .frame(width: 300,  height: 50)
+                    .overlay {RoundedRectangle(cornerRadius: 10).stroke(Color.primary)}
+                    .border(Color.red, width: CGFloat(wrongPassword)).cornerRadius(10)
+                    .autocapitalization(.none)
+                
+                Button("Login") {
+                    authenticateUser(username: username, password: password)
+                }
+                    .foregroundColor(Color.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+
+                    
+                Spacer()
+                
+                NavigationLink(value: isLoggedIn) {
+                    EmptyView()
+                }
+                .navigationDestination(isPresented: $isLoggedIn) {
+                    Text("You are logged in @\(username)")
+                }
+            }
+
+        }
+        .navigationBarHidden(true)
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "test" {
+            wrongUsername = 0
+            if password.lowercased() == "password" {
+                wrongPassword = 0
+                isLoggedIn = true
+            } else {
+                wrongPassword = 4
+            }
+        } else {
+            wrongUsername = 4
         }
     }
 }
